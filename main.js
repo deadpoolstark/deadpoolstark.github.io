01315bd01842cac0069aeeb02f9ab88e0dc07192
deadpoolstark3 <deadpoolstark3@gmail.com>
2023-02-20 02:18:11 +0530

    removed blur. fuck blur. fuck blur. fuck blur. fuck blur. fuck blur. fuck blur

--- main.js
+++ main.js
@@ -1,27 +1,12 @@
-console.log("works?")
 document.addEventListener('DOMContentLoaded', () => {
-  const blob = document.getElementById('blob');
-  console.log(blob);
+  const blob = document.getElementById('blob')
+  console.log(blob)
   document.body.onpointermove = event => {
-    const { clientX, clientY } = event;
-    const scrollX = window.pageXOffset;
-    const scrollY = window.pageYOffset;
+    const { clientX, clientY } = event
     blob.animate(
       {
-        left: `${clientX + scrollX}px`,
-        top: `${clientY + scrollY}px`
-      },
-      {
-        duration: 3000,
-        fill: "forwards"
-      }
-    );
-  };
-  window.addEventListener('scroll', () => {
-    if (window.scrollY >= pageHeight) {
-      blob.style.animationPlayState = 'paused';
-    } else {
-      blob.style.animationPlayState = 'running';
-    }
-  });
+      left: `${clientX}px`,
+      top: `${clientY}px`},
+      {duration:3000,fill:"forwards"}
+      );}
 });
