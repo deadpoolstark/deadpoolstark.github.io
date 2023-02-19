e2f98ea2e9ff35c68f4b355b2debd3238919016d
deadpoolstark3 <deadpoolstark3@gmail.com>
2023-02-20 01:51:58 +0530

    ADDED THE HOVER EFFECT HELL YEAH BITCHESSS

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
-const blob = document.getElementById("blob");
-document.body.onpointermove= event => {
-    const{ clientX,clientY}=event;
-
-    blob.style.left='${clientX}px';
-    blob.style.top='${clientY}px';
-}
+document.addEventListener('DOMContentLoaded', () => {
+  const blob = document.getElementById('blob');
+  console.log(blob);
+  document.body.onpointermove = event => {
+    const { clientX, clientY } = event;
+    const scrollX = window.pageXOffset;
+    const scrollY = window.pageYOffset;
+    blob.animate(
+      {
+        left: `${clientX + scrollX}px`,
+        top: `${clientY + scrollY}px`
+      },
+      {
+        duration: 3000,
+        fill: "forwards"
+      }
+    );
+  };
+  window.addEventListener('scroll', () => {
+    if (window.scrollY >= pageHeight) {
+      blob.style.animationPlayState = 'paused';
+    } else {
+      blob.style.animationPlayState = 'running';
+    }
+  });
+});
